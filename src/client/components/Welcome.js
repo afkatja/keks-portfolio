4bfb158765c767b04665389ef5622db04db74a6b
Katja Hollaar <afkatja@gmail.com>
2017-12-11 15:32:48 +0100

    Learning context, HOC, compound components

--- src/client/components/Welcome.js
+++ src/client/components/Welcome.js
@@ -3,6 +3,7 @@ import { FormattedRelative, FormattedMessage } from 'react-intl';
 import moment from 'moment';
 
 import ErrorBoundary from './ErrorBoundary';
+import Toggle from './atoms/Toggle/Toggle';
 
 export default function Welcome() {
   return (
@@ -19,10 +20,30 @@ export default function Welcome() {
                 value={moment().subtract(1, 'days')}
                 options={{ style: 'numeric', units: 'second' }}
               />
-            )
+            ),
           }}
         />
       </h1>
+      <Toggle
+        defaultOn
+        onReset={on => console.log('reset state to ', on)}
+        render={({ ...toggle }) =>
+          (<div>Toggle is {toggle.on}
+            <button
+              {...toggle.getTogglerProps({
+                onClick: () => console.log('another click handler', toggle.on),
+                id: 'btnId',
+              })}
+            >{toggle.on ? 'on' : 'off'}</button>
+            <button onClick={() => toggle.reset()}>Reset</button>
+          </div>)
+        }
+      />
+      {/* <Toggle onToggle={on => console.log('toggled', on)}>
+        <Toggle.On>The button in on</Toggle.On>
+        <Toggle.Off>The button is off</Toggle.Off>
+        <Toggle.Button />
+      </Toggle> */}
     </ErrorBoundary>
   );
 }
--- src/client/components/atoms/Toggle/Toggle.js
+++ src/client/components/atoms/Toggle/Toggle.js
@@ -1,47 +1,82 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import hoistNonReactStatic from 'hoist-non-react-statics';
 
-function ToggleOn({ on, children }) {
-  return on ? children : null;
-}
-function ToggleOff({ on, children }) {
-  return on ?  null : children;
-}
-function ToggleButton({ on, toggle, ...props }) {
-  return (
-    <button type="button" onClick={toggle} {...props} />
-  );
-}
-export default class MyComponent extends Component {
-  static defaultProps = {
-    onToggle: () => {},
+const TOGGLE_CONTEXT = '__toggle__';
+
+export const withToggle = (Comp) => {
+  function Wrapper(props, context) {
+    const toggleContext = context[TOGGLE_CONTEXT];
+    return (<Comp {...props} {...toggleContext} />);
+  }
+  Wrapper.contextTypes = {
+    [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
   };
-  static On = ToggleOn;
-  static Off = ToggleOff;
-  static Button = ToggleButton;
-  
+  Wrapper.displayName = `withToggle(${Comp.displayName || Comp.name})`;
+  Wrapper.WrappedComponent = Comp;
+  return hoistNonReactStatic(Wrapper, Comp);
+};
+
+const renderSwitch = ({ on, toggle, ...props }) => (<button type="button" onClick={toggle} {...props}>Toggle {on}</button>);
+
+const ToggleOn = ({ on, children }) => on ? children : null; // eslint-disable-line
+const ToggleOff = ({ on, children }) => !on ? children : null; // eslint-disable-line
+const ToggleButton = ({ on, toggle, ...props }) => renderSwitch({
+  on,
+  toggle,
+  ...props,
+});
+
+const compose = fns => args => console.log(fns) || fns.forEach(fn => fn && fn(...args)); //eslint-disable-line
+
+export default class Toggle extends Component {
+  static defaultProps = {
+    defaultOn: false,
+    onToggle: () => {},
+    onReset: () => {},
+  };
+  static childContextTypes = {
+    [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+  };
+  static On = withToggle(ToggleOn);
+  static Off = withToggle(ToggleOff);
+  static Button = withToggle(ToggleButton);
+
   constructor(props) {
     super(props);
-
-    this.state = {
-      on: false,
+    this.initialState = {
+      on: this.props.defaultOn,
     };
+    this.state = this.initialState;
     this.toggle = () => this.setState(
-      ({ on }) => { on: !on}),
-      () => this.props.onToggle(this.state.on)
+      ({ on }) => ({ on: !on }),
+      () => this.props.onToggle(this.state.on),
     );
+
+    this.reset = () => this.setState(
+      this.initialState,
+      () => this.props.onReset(this.state.on),
+    );
+    this.getChildContext = () => ({
+      [TOGGLE_CONTEXT]: {
+        on: this.state.on,
+        toggle: this.toggle,
+      },
+    });
+
+    this.getTogglerProps = ({ onClick, ...renderProps } = {}) => ({
+      'aria-expanded': this.state.on,
+      onClick: compose(onClick, this.toggle),
+      ...renderProps,
+    });
   }
 
   render() {
-    const children = React.Children.map(
-      this.props.children,
-      child => React.cloneElement(child, {
-        on: this.state.on,
-        toggle: this.toggle,
-      })
-    );
-    return (
-      <div>{children}</div>
-    );
+    return this.props.render({
+      on: this.state.on,
+      toggle: this.toggle,
+      reset: this.reset,
+      getTogglerProps: this.getTogglerProps,
+    });
   }
-
 }
